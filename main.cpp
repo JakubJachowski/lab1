fe277946a74d801a8279d9a9105bd4e83a1a9a98
JakubJachowski <226111@student.pwr.edu.pl>
2016-03-19 21:26:22 +0100

    jeden rabin powie ze ok, drugi ze ta petla troche *******

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void drawCharSquare(char **square, int n);
 int main(int argc, char **argv)
 {
 	int n=atoi(argv[1]);
-	srand(time(NULL));
+	srand(atoi(argv[2]));
 	drawCharSquare(charSquare(n), n);	
 }
 
@@ -20,12 +20,11 @@ char **charSquare(int n)
 	for(int i=0;i<n;i++)
 		square[i] = new char[n];
 
-	for(int i=0;i<n;i++)
-		for(int j=0;j<n;j++)
-		{
-			square[i][j]=97+rand()%27;
-			printf("%c",square[i][j]);
-		}
+	for(int i=0,j=0;(i<n)||(j<n);j++)
+	{
+			square[i][j%n]=97+rand()%26;
+			if((j%n)==n-1) i++;
+	}
 
 	return square;
 
@@ -33,5 +32,13 @@ char **charSquare(int n)
 
 void drawCharSquare(char **square, int n)
 {
-
+	for(int i=0,j=0;(i<n)||(j<n);j++)
+	{
+			printf("%c ", square[i][j%n]);
+			if((j%(n))==n-1)
+			{
+				i++;
+				printf("\n");
+			}
+	}
 }
