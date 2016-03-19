4ee328037c2215d3f57c69efcc8fafcd8553ae61
JakubJachowski <226111@student.pwr.edu.pl>
2016-03-19 20:43:22 +0100

    wuj wie czy to bedie ok

--- main.cpp
+++ main.cpp
@@ -7,14 +7,28 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	int n=atoi(argv[1]);
+	srand(time(NULL));
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;//i czu to nie są jaja
+	char **square;
+	square = new char *[n];
+
+	for(int i=0;i<n;i++)
+		square[i] = new char[n];
+
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+		{
+			square[i][j]=97+rand()%27;
+			printf("%c",square[i][j]);
+		}
+
 	return square;
+
 }
 
 void drawCharSquare(char **square, int n)
